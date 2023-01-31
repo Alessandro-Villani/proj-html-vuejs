e11b405102b8f8f901defc0ec8f441e0395d341e
Alessandro-Villani <alessvill@libero.it>
2023-01-31 12:08:55 +0100

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,201 @@
+const headerMenus = [
+    {
+        title: 'home',
+        links: ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+    },
+    {
+        title: 'pages',
+        links: ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+    },
+    {
+        title: 'courses',
+        links: ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+    },
+    {
+        title: 'features',
+        links: ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+    },
+    {
+        title: 'blog',
+        links: ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+    },
+    {
+        title: 'shop',
+        links: ['link 1', 'link 2', 'link 3', 'link 4', 'link 5']
+    }
+];
+
+const coachingCards = [
+    {
+        title: 'online courses',
+        imgUrl: 'artist-box-image-01.png',
+        text: 'Online art coaching now offers you a very powerful way to redesign your artist mind.' 
+    },
+    {
+        title: 'one to one',
+        imgUrl: 'artist-box-image-02.png',
+        text: 'Getting the necessary clarity about the current state to help you improve your ability.' 
+    },
+    {
+        title: 'anywhere',
+        imgUrl: 'artist-box-image-03.png',
+        text: 'Access to valuable and portable program which allow you to setup and live anywhere you want.' 
+    },
+    {
+        title: 'on time',
+        imgUrl: 'artist-box-image-04.png',
+        text: "Punctuality is cxjr top priority because it's an essential criteria to assess a program quality."
+    },
+
+];
+
+const testimonialCards = [
+    {
+        title: 'Professional team of specialists and passionate mentors at reach',
+        review: 'I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.',
+        name: 'Madley Pondor',
+        role: 'IT Specialist', 
+        imgUrl: 'artist-testimonial-avatar-01'
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos recusandae, dolorum placeat est aspernatur expedita rem quaerat quo, ducimus repudiandae ipsa eius quidem iure cum, culpa officia eum sed repellat.',
+        name: 'Lorem Ipsum',
+        role: 'Artist', 
+        imgUrl: 'artist-testimonial-avatar-02'
+    },
+    {
+        title: "It's a choice of quality for people with special needs",
+        review: 'Ilm a very strict person so I require everything to be right and shine. MaxCoach guys just got me.',
+        name: 'Florence Themes',
+        role: 'Multimedia Admin', 
+        imgUrl: 'artist-testimonial-avatar-03'
+    },
+    {
+        title: 'High level of efficiency and scientific teaching methods',
+        review: 'I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.',
+        name: 'Mina Hollace',
+        role: 'freelancer', 
+        imgUrl: 'artist-testimonial-avatar-04'
+    },
+];
+
+const artistCards = [
+    {
+        title: 'the acrylic painting academy',
+        price: 18,
+        lessons: 4,
+        students: 50, 
+        imgUrl: 'artist-course-08-480x480.jpg'
+    },
+    {
+        title: 'drawing and shading complete course',
+        price: 21,
+        lessons: 14,
+        students: 50, 
+        imgUrl: 'artist-course-07-480x480.jpg'
+    },
+    {
+        title: 'the color theory for digital artist',
+        price: 19,
+        lessons: 7,
+        students: 50, 
+        imgUrl: 'artist-course-06-480x480.jpg'
+    },
+    {
+        title: 'ultimate guide to digital sketching for beginner',
+        price: 35,
+        lessons: 14,
+        students: 50, 
+        imgUrl: 'artist-course-05-480x480.jpg'
+    },
+    {
+        title: 'portrait drawing the smart way',
+        price: 19,
+        lessons: 2,
+        students: 50, 
+        imgUrl: 'artist-course-04-480x480.jpg'
+    },
+    {
+        title: 'mastering watercolor painting from beginner',
+        price: 19,
+        lessons: 9,
+        students: 50, 
+        imgUrl: 'artist-course-03-480x480.jpg'
+    },
+    {
+        title: 'the art & science of drawing',
+        price: 25,
+        lessons: 4,
+        students: 50, 
+        imgUrl: 'artist-course-02-480x480.jpg'
+    },
+    {
+        title: 'the colored pencil drawing course',
+        price: 6,
+        lessons: 6,
+        students: 50, 
+        imgUrl: 'artist-course-01-480x480.jpg'
+    }, 
+];
+
+const articleCards = [
+    {
+        title: 'pocket-sized notebooks hold miniature paintings',
+        date: new Date('05-15-2020'),
+        views: 603, 
+        imgUrl: 'artist-blog-01-480x356.jpg'
+    },
+    {
+        title: 'connection between self-portraits and identity',
+        date: new Date('05-15-2020'),
+        views: 377, 
+        imgUrl: 'artist-blog-02-500x680.jpg'
+    },
+    {
+        title: 'brush strokes energize trees in paintings',
+        date: new Date('05-15-2020'),
+        views: 688, 
+        imgUrl: 'artist-blog-03-480x356.jpg'
+    }  
+];
+
+const eventCards = [
+    {
+        title: 'storytelling workshop',
+        date: new Date('11-22-2020'),
+        location: 'Texas, US', 
+        imgUrl: 'artist-event-04-250x300.jpg'
+    },
+    {
+        title: 'painting art contest 2020',
+        date: new Date('10-10-2020'),
+        location: 'New York, US', 
+        imgUrl: 'artist-event-03-250x300.jpg'
+    },
+    {
+        title: 'internal art fair 2020',
+        date: new Date('11-23-2020'),
+        location: 'Hamburg, Germany', 
+        imgUrl: 'artist-event-02-250x300.jpg'
+    },
+    {
+        title: 'street performance: call for artist',
+        date: new Date('12-15-2020'),
+        location: 'Illinois, US', 
+        imgUrl: 'artist-event-01-250x300.jpg'
+    }
+];
+
+const footerMenus = [
+    {
+        title: 'explore',
+        links: ['start here', 'success story', 'blog', 'courses', 'contact us']
+    },
+    {
+        title: 'information',
+        links: ['membership', 'purchase guide', 'privacy policy', 'terms of service']
+    },
+]
+
+export {headerMenus, coachingCards, testimonialCards, artistCards, articleCards, eventCards, footerMenus}
